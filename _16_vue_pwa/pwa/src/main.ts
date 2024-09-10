806d2b4caa748a45748b08895e9ee6dbea8172bf
Juan Yepes <juanferhiguita65@gmail.com>
2024-09-10 13:52:47 -0500

    Add: PWA class finish

--- _16_vue_pwa/pwa/src/main.ts
+++ _16_vue_pwa/pwa/src/main.ts
@@ -4,3 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+//Para probar el pwa se instala un servidor http: npm install http-server -g, luego se inicia el servidor http: http-server dist/ -- carpeta de distribucion que hicimos anteriormente con npm run build
+
+//Usando ngrok ejecutamos la app, ya que las pwa solo funcionan en https: ngrok.exe http 8080
+
+//Luego usando el manifest que movemos del dist a la carpeta public podemos configurar lo visual, igual que en el vue.config.js
--- _16_vue_pwa/pwa/vue.config.js
+++ _16_vue_pwa/pwa/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'PWA APP',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 })
