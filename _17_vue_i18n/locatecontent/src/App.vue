72f82afe437272ae7dbf487e641ff6e17fae9eaf
Juan Yepes <juanferhiguita65@gmail.com>
2024-09-11 14:14:42 -0500

    vue i18 class created

---
+++ _17_vue_i18n/locatecontent/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <h1>hola mundo</h1>
+  <h2>Hello world</h2>
+
+  <p>Hello world with i18 vue package</p>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
