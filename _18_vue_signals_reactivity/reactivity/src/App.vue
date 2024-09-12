a064796050809dc0150fb0cc81c1d0cdfe1563c7
Juan Yepes <juanferhiguita65@gmail.com>
2024-09-11 19:54:53 -0500

    Signals, reactive, plugin, global

---
+++ _18_vue_signals_reactivity/reactivity/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <h1>SOLID REACTIVITY</h1>
+  <h2>{{ count }}</h2>
+
+  <button @click="setCount((v) => v + 1)">+ + +</button>
+</template>
+
+<script lang="ts" setup>
+//Existen señales solid y angular
+  import { createSignal } from 'vue-signals/solid'
+
+  //valor ,variable para modificar este valor
+  const [ count, setCount ] = createSignal(0)
+
+</script>
+
+<style>
+
+</style>
